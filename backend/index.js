6e1e8b5c232f648b08ed1cc1d21ed3f8b9f39bff
Fady Dib <fady.dib@hotmail.com>
2023-04-02 01:10:38 +0300

    Add auth routes

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,9 @@ console.log(process.env.PORT)
 
 const port = process.env.PORT || 3002
 
+const authRouter = require("./routes/auth.routes")
+app.use('auth', authRouter)
+
 app.listen(port, (err) => {
     if (err) console.log(err)
     console.log("Server is running on port ", port);
