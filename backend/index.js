61fad128fef3ae4f836ccf0529b6fdfa3b2d0d05
Fady Dib <fady.dib@hotmail.com>
2023-04-01 23:22:38 +0300

    Add user schema

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-require("./config/db.config")
+
+
 console.log(process.env.PORT)
 
 const port = process.env.PORT || 3002
 
-app.listen(port, () => {
+app.listen(port, (err) => {
+    if (err) console.log(err)
     console.log("Server is running on port ", port);
+    require("./config/db.config")
 })
