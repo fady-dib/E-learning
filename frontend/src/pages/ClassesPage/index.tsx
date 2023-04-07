64cc1a89ba08400c1b636f0738d4756c3d538fbb
Fady Dib <fady.dib@hotmail.com>
2023-04-07 04:03:32 +0300

    Configure docker

--- frontend/src/pages/ClassesPage/index.tsx
+++ frontend/src/pages/ClassesPage/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../../components/Navbar';
 import axios from 'axios';
-import Button from "../../components/Button";
 import Class from '../../components/Class';
 import "../../components/Class/index.css"
 
@@ -9,7 +8,7 @@ import "../../components/Class/index.css"
 interface ClassData {
     name: string;
     instructor: string;
-  
+
 }
 
 const ClassesPage = () => {
@@ -17,20 +16,22 @@ const ClassesPage = () => {
     const [Classes, setClasses] = useState([]);
 
     const token = localStorage.getItem('token');
-  
+
+    const getClasses = () => {
+        axios.get('http://localhost:3002/class/classes', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            setClasses(response.data)
+        })
+    }
+
 
     useEffect(() => {
-        const getClasses = () => {
-            axios.get('http://localhost:3002/class/classes', {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            }).then(response => {
-                setClasses(response.data)
-            })
-        }
+      
         getClasses()
-    }, [])
+    },)
 
 
 
@@ -39,19 +40,19 @@ const ClassesPage = () => {
         <>
             <div>
                 <Navbar></Navbar>
-                
+
                 <div className='classes-content'>
-                <h2 className='list'>Classes List</h2>
+                    <h2 className='list'>Classes List</h2>
                     <div className="table-container">
-                    {Classes.length > 0 ? (
-            Classes.map((classData, index) => (
-              <React.Fragment key={index}>
-                <Class key={classData} classData={classData} />
-              </React.Fragment>
-            ))
-          ) : (
-            <p>No classes found.</p>
-          )}
+                        {Classes.length > 0 ? (
+                            Classes.map((classData, index) => (
+                                <React.Fragment key={index}>
+                                    <Class key={classData} classData={classData} />
+                                </React.Fragment>
+                            ))
+                        ) : (
+                            <p>No classes found.</p>
+                        )}
 
                     </div>
                 </div>
